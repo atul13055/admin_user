3a5a9e89ab841fa620fa127151b0594f7c94f5a6
atul yadav <atulyadav9039@gmail.com>
2025-01-30 19:15:27 +0530

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.find_or_create_by(email: 'admin@example.com') do |user|
+  user.password = 'password123'
+  user.password_confirmation = 'password123'
+end
